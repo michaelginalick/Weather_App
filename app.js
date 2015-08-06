05793ee5f0631b75e6d20c51045d6480f516b76a
Michael <Michael.ginalick@gmail.com>
2015-08-05 20:59:32 -0500

    added API call and data binding

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ weatherApp.config(function ($routeProvider){
         
     })
     
+    .when('/forcast/:days', {
+        controller:  'forcastController',
+        templateUrl: '/pages/forcast.htm'
+        
+    })
+    
 });
 
 
@@ -37,8 +43,23 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     });
 }]);
 
-weatherApp.controller('forcastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forcastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
     $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK" }, {get: {method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    
+    $scope.convertToFarenheit = function(degk) {
+        return Math.round((1.8 * (degk - 273)) + 32);
+    }
+    
+    $scope.convertToDate = function(dt) {
+      return new Date(dt * 1000)  
+    };
     
 }]);
 
